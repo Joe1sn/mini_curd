9713f4ee0c017b70a46e1ac000aca2f29df99c08
Joe1sn <joe1sn23333@gmail.com>
2022-12-01 20:04:04 +0800

    finished search index

--- app_curd/views.py
+++ app_curd/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, redirect,HttpResponse
 from django.contrib.auth.decorators import login_required
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger, InvalidPage
+from app_curd.utils.paginator import Paginator, EmptyPage, PageNotAnInteger, InvalidPage
 from app_curd import models
 from app_curd.utils.fuzz_search import search
-import time
+from math import *
 
 @login_required(login_url='/login/')
 def index(request):
@@ -20,24 +20,54 @@ def get_page(request):
         query = ""
     
     #根据 `objs` 生成多页
-    # 使用request.GET.get()函数获取url中的page参数的数值。默认第1页
+    # 使用request.GET.get()函数获取url中的page参数的数值。默认第1页 ceil(objs.count()/pre_page)
     try:
         page = request.GET.get('page')
     except:
-        page = 1
+        page = "1"
+    if page == None:
+        page = "1"
+    page_num = int(page)    #当前页数
+    pre_page = 3            #每页元素数目
+    slice_max = 5           #最大同时页数
+    max_page = 1            #最大页数
+    min_page = 1            #最小页数
+    not_reach_max_flag = True  #同时页数是否超过最大页
+    # 查询结果并分页 
+    # 生成Paginator对象对数据分页，每页显示x条数据
     if query != None:
         objs = search(query)    #查询
-    else:
-        objs = []
-    # 生成Paginator对象对数据分页，每页显示x条数据
-    paginator = Paginator(objs,1)
+        max_page = ceil(objs.count()/pre_page)
+        if slice_max > max_page:
+            slice_max = max_page
+            not_reach_max_flag = False
+            paginator = Paginator(objs,pre_page)
 
+        elif page_num < ceil(slice_max/2):
+            paginator = Paginator(objs,pre_page,start=1,end=slice_max)
+        elif page_num <= max_page - ceil(slice_max/2):
+            paginator = Paginator(objs,pre_page,start=page_num-int(slice_max/2),end=page_num+int(slice_max/2))
+        else:
+            paginator = Paginator(objs,pre_page,start=max_page-slice_max+1,end=max_page)
+
+    else:
+        objs = []#paginator.num_pages
+        paginator = Paginator(objs,1)
+        
+    # paginator = Paginator(objs,1)
     # 获取查询页数的接口数据列表，page()函数会判断page实参是否是有效数字
     try:
         page_obj = paginator.page(page)
+        
     except PageNotAnInteger:
         page_obj = paginator.page(1)
     except (EmptyPage, InvalidPage):
         # 创建最终的page对象
         page_obj = paginator.page(paginator.num_pages)
-    return render(request,"get_page.html",{"objs":page_obj,})
+    print(page_obj.count)
+    return render(request,"get_page.html",{
+        "objs":page_obj,
+        "max_page":max_page-ceil(slice_max/2),
+        "min_page":min_page+ceil(slice_max/2),
+        "not_reach_max":not_reach_max_flag
+    })
